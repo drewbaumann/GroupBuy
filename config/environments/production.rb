cb795c3c15973ef210f803c031094660d4040ff0
Drew Baumann <drewbaumann@gmail.com>
2012-05-12 20:05:32 -0700

    got rid of the old DB

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,8 @@ GroupBuy::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.precompile += %w( *.css *.js )
+  
 
   # Generate digests for assets URLs
   config.assets.digest = true
